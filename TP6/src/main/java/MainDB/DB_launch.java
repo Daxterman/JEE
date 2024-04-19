4c7e63527ff0e24c47ead158ea2c5410fdae9271
Daxter <daxterman62@gmail.com>
2024-04-19 11:44:26 +0200

    Import DB -> webapp

--- TP6/src/main/java/MainDB/DB_launch.java
+++ TP6/src/main/java/MainDB/DB_launch.java
@@ -1,10 +1,11 @@
 package MainDB;
+import jcbcPack.jcbcMain;
 
 public class DB_launch {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		jcbcMain.main(args);
 	}
 
 }
